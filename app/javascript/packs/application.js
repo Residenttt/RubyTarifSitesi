022968593baed180779ed31a4d57b2a33cb77b8f
Residenttt <ilkersari796@gmail.com>
2021-01-22 19:12:21 +0300

    Bootstrap5-Jscript fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,5 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+import "bootstrap"
+import "../stylesheets/application"
