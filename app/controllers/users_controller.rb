97a9904928cc286ab1eae025b3565a4d5d5bfddd
Residenttt <ilkersari796@gmail.com>
2021-01-24 00:07:21 +0300

    Login-konu editleme-silme yetkilendirmesi eklendi. Tarif içeriği frontend düzenlendi.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,59 @@
+class UsersController < ApplicationController
+  include Pagy::Backend
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
+  def index
+    @pagy, @users = pagy(User.all, items: 10)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:danger] = "Üye silindi!"
+    redirect_to users_path
+  end
+
+    private
+      def set_user
+        @user = User.find(params[:id])
+      end
+
+      def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+      end
+
+      def require_same_user
+        if current_user != @user
+          redirect_to users_path
+        end
+      end
+
+end
