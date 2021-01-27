7c1edee2ebc61a5e02f0ba301ded19676a3f9b7c
Residenttt <ilkersari796@gmail.com>
2021-01-28 01:27:19 +0300

    Görsel update//fontawesome, google fonts eklendi. Sayfalar düzenlendi kodlar gözden geçirildi.

---
+++ app/controllers/yorumlars_controller.rb
@@ -0,0 +1,21 @@
+class YorumlarsController < ApplicationController
+  before_action :require_user
+
+  def create
+    @tarifler = Tarifler.find(params[:tarifler_id])
+    @yorumlar = @tarifler.yorumlars.build(yorumlar_params)
+    @yorumlar.user = current_user
+    if @yorumlar.save
+      #ActionCable.server.broadcast "yorumlar", render(partial: 'yorumlars/yorumlar', object: @yorumlar)
+      redirect_to tarifler_path(@tarifler)
+    else
+      redirect_back fallback_location: tarifler_path(@tarifler)
+    end
+  end
+
+  private
+
+  def yorumlar_params
+    params.require(:yorumlar).permit(:aciklama)
+  end
+end
