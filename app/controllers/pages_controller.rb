f259f75dd97115b8ecf5e48b66363fa4b86360b0
Residenttt <ilkersari796@gmail.com>
2021-01-28 20:03:52 +0300

    Ana sayfa düzenlemesi.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
     def index
         # eğer giriş yapılmışsa direkt tarifler sayfasına gönderecek ana sayfa
-        redirect_to tariflers_path if logged_in?
+        #redirect_to tariflers_path if logged_in?
     end
 
 end
